846d0c7ac7c7acb58d4bdc5fc940b91f5c5fc564
Daniel Abrahamberg <daniel@zenithsystem.se>
2019-06-11 20:46:07 +0200

    4X4 Click only posiible choices- correct randome

--- src/App.js
+++ src/App.js
@@ -2,56 +2,66 @@ import React ,{useState} from 'react';
 import GameCell from './components/GameCell.js'
 import './App.css';
 
-const CalculateClickabels=(a)=>{
+const calculateClickabels=(a)=>{
   const ind = a.indexOf(0); 
-  const vert = Math.floor(ind /3)  ;
-  const horz = ind % 3 ;
-  console.log('v',vert,'h',horz);
-  return [1,3,4];
+  const vert = Math.floor(ind /4)  ;
+  const horz = ind % 4 ;
+
+  let clickabelsIndx=[];
+  let clickabels=[];
+  if(horz!==0)
+    clickabelsIndx.push(ind -1);
+  if(horz!==3)
+    clickabelsIndx.push(ind + 1);
+  if(vert!==0)
+    clickabelsIndx.push(ind -4);
+  if(vert!==3)
+    clickabelsIndx.push(ind + 4);
+
+    clickabelsIndx.map( x=> clickabels.push(a[x]));
+ 
+  return clickabels;
 }
 
-const CalculateNewArry=(a,num)=>{
+const switchPlace=(a,num)=>{
   const ind0 = a.indexOf(0); 
   const indNum = a.indexOf(num);
   let newA = [...a];
   newA[ind0] = num;
   newA[indNum] = 0;
-  console.log('newa',newA);
+
   return newA;
 }
 
-const shuffle=(array)=> {
-  var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+const shuffle=(array)=>{
+  let randomeMoves = Math.floor(Math.random() * 800) + 300; //Decide how many times you want to shaffle
+  let currentArry = [...array];
+  for (let i = 0; i < randomeMoves; i++) { 
+    let possibleNumbers = calculateClickabels(currentArry);
+    let chosenNo = randomeToSwap(possibleNumbers);
+    currentArry = switchPlace(currentArry,chosenNo);
   }
+  return currentArry;
 
-  return array;
-}
+};
+
+const randomeToSwap =(arry)=>  arry[Math.floor(Math.random()*arry.length)];
+const range = (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i);
 
 
 
 function App() {
-const [nums,setNums] = useState( shuffle(utils.range(0,8)) );
-const [clickabels,setClickabels] = useState(CalculateClickabels(nums));
+const [nums,setNums] = useState( shuffle(range(0,15)) );
 
 const CellClicked =(x)=> {
-  // if(clickabels.includes(x)) 
-  // console.log({x},'clicked');
-  setNums(CalculateNewArry(nums,x));
+  
+   const clickabels= calculateClickabels(nums);
+  if(clickabels.includes(x)) 
+    setNums(switchPlace(nums,x));
 }
 
-console.log(nums);
+
   return (
     <div className="App">
      <div className="GameSpace">
@@ -61,36 +71,5 @@ console.log(nums);
   );
 }
 
-
-
-// Math science
-const utils = {
-  // Sum an array
-  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
-
-  // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i),
-
-  // pick a random number between min and max (edges included)
-  random: (min, max) => min + Math.floor(max * Math.random()),
-
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr, max) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
-        }
-      }
-    }
-    return sums[utils.random(0, sums.length)];
-  },
-};
 
 export default App;
